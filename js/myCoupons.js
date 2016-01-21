da6db946336e5896cbac6971f0a81a6d619770be
Devil <939730376@qq.com>
2016-01-21 18:14:56 +0800

    增加了技师列表页

---
+++ js/myCoupons.js
@@ -0,0 +1,30 @@
+
+var userImage = document.querySelector('.user-image'),
+	popupClose = document.querySelector('.popup-close'),
+	btnSubmit = document.querySelector('.submit');
+
+
+
+// 弹窗--绑定手机
+userImage.addEventListener('tap',function(){
+	$('.popup-validate').show();
+});
+
+// 关闭弹窗-绑定手机
+popupClose.addEventListener('tap',function(){
+	$('.popup-validate').hide();
+});
+
+
+// 提交绑定手机信息
+btnSubmit.addEventListener('tap',function(){
+	$('.popup-validate').hide();
+});
+
+// 跳转页面
+
+$('.box-coupons').on('tap','.box-coupons-can-use',function(){
+	mui.openWindow({
+	    url:'couponsDetailed.html'
+	});
+});
